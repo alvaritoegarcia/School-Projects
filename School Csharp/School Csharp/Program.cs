b1ec9d68e97154bf5e2163aec94a219328dc48a4
alvaritoegarcia <alvaroenrique.gar2005@gmail.com>
2024-03-11 19:07:58 -0300

    first commit

---
+++ School Csharp/School Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+String[] names = new string[10];
+
+for (int x=0; x < 10; x++)
+{
+    Console.WriteLine(" Escriba su nombre");
+    names[x] = Console.ReadLine();
+}
+
+foreach (string x in names)
+    Console.WriteLine(x);
